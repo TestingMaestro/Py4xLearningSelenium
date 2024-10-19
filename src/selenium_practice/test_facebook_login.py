6c225dd1ff728ebbe3c266a2970071b348dd38d6
Yashodhar Karki <yashodhar.karkii@gmail.com>
2024-10-19 05:39:04 +0530

    Exercises and Tasks of OCT 17, 2024 are added with few practices

---
+++ src/selenium_practice/test_facebook_login.py
@@ -0,0 +1,14 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_fb_login():
+    driver = webdriver.Chrome()
+    driver.get("https://www.facebook.com/")
+    user_email = driver.find_element(By.NAME, value="email")
+    user_email.send_keys("lihilfilsj")
+    user_pass = driver.find_element(By.NAME, value="pass")
+    user_email.send_keys("lihilfilsj")
+    time.sleep(9)
--- src/tasks_17102024/test_katalon_cura.py
+++ src/tasks_17102024/test_katalon_cura.py
@@ -13,11 +13,18 @@ def test_Katalon_cura_demo_login_tc_chrome(browser_options):
     make_appointment_btn = driver.find_element(By.ID, value="btn-make-appointment")
     make_appointment_btn.click()
     current_url = driver.current_url
+    print(current_url)
+    time.sleep(6)
     assert current_url == "https://katalon-demo-cura.herokuapp.com/profile.php#login"
+
     user_name = driver.find_element(By.NAME, value="username")
     user_name.send_keys("John Doe")
     password = driver.find_element(By.NAME, value="password")
     password.send_keys("ThisIsNotAPassword")
-    login_btn = driver.find_element(By.ID,value="btn-login")
+    login_btn = driver.find_element(By.ID, value="btn-login")
     login_btn.click()
-    time.sleep(8)
+    time.sleep(6)
+    home_page_url = driver.current_url
+    print(home_page_url)
+    assert home_page_url == "https://katalon-demo-cura.herokuapp.com/#appointment"
+    time.sleep(10)
